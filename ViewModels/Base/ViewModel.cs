b82b66e4ffa32bfdb869fdb703b9de76ab06d12c
Dmitry Pogonyiaev <lounge000@mail.ru>
2021-08-16 19:21:54 +0300

    Add project wpf+ mVvM

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace BoxBoost.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // CallerMemberName - получаем имя метода откуда вызвали
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false; // если значение поля уже имеет значение - выходим
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using BoxBoost.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxBoost.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        /// <summary>Заголовок</summary>
+        private string _Title = "test";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        /// <summary> Статус операции </summary>
+        private string _Status = "Ready";
+
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+    }
+}
