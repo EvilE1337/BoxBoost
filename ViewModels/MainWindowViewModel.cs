c93e915b0ee33e879c5e2c03d23120cc98af9462
Dmitry Pogonyiaev <lounge000@mail.ru>
2021-08-17 10:07:28 +0300

    Exit Application

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,18 @@
-﻿using BoxBoost.ViewModels.Base;
+﻿using BoxBoost.Infrastructure.Commands;
+using BoxBoost.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BoxBoost.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
         /// <summary>Заголовок</summary>
         private string _Title = "test";
 
@@ -17,7 +21,9 @@ namespace BoxBoost.ViewModels
             get => _Title;
             set => Set(ref _Title, value);
         }
+        #endregion
 
+        #region Статус операции
         /// <summary> Статус операции </summary>
         private string _Status = "Ready";
 
@@ -25,6 +31,32 @@ namespace BoxBoost.ViewModels
         {
             get => _Status;
             set => Set(ref _Status, value);
+        }
+        #endregion
+
+        #region Команды
+
+        #region Закрытие приложения
+        public ICommand CloseApplicationCommand { get; set; }
+
+        private void OnCloseApplicationCommandExecute(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
+
+            #endregion
         }
     }
 }
