18f70776baf3a74459e6febf2d8340f4761e4cea
Dmitry Pogonyiaev <lounge000@mail.ru>
2021-09-13 08:40:48 +0300

    Окно с LP

--- ViewModels/SettingsLocalProxyViewModel.cs
+++ ViewModels/SettingsLocalProxyViewModel.cs
@@ -1,13 +1,20 @@
-﻿using BoxBoost.ViewModels.Base;
+﻿using BoxBoost.Infrastructure.Commands;
+using BoxBoost.ViewModels.Base;
+using BoxBoost.ViewModels.VMSettings;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Serialization;
 
 namespace BoxBoost.ViewModels
 {
-    public class SettingsLocalProxyViewModel : ViewModel
+    public class SettingsLocalProxyViewModel : LocalProxySettings
     {
         #region Controls
 
@@ -15,13 +22,52 @@ namespace BoxBoost.ViewModels
 
         #region Commands
 
+        #region Открыть файл
+        [XmlIgnore]
+        public ICommand OpenFileCommand { get; set; }
+
+        private void OnOpenFileCommandExecute(object p)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] stringSeparators = new string[] { "\r\n" };
+                string[] lines = File.ReadAllText(openFileDialog.FileName).Split(stringSeparators, StringSplitOptions.None);
+
+                foreach (string el in lines)
+                {
+                    if(!string.IsNullOrEmpty(el) && ListProxyFileItem.IndexOf(el) == -1)
+                        ListProxyFileItem.Add(el);
+                }
+            }
+        }
+
+        private bool CanOpenFileCommandExecute(object p) => true;
+
+        #endregion
+
+        #region Очистить список
+        [XmlIgnore]
+        public ICommand ClearListCommand { get; set; }
+
+        private void OnClearListCommandExecute(object p) => ListProxyFileItem.Clear();
+
+        private bool CanClearListCommandExecute(object p) => true;
+
+        #endregion
+
         #endregion
 
         #region constructor
 
         public SettingsLocalProxyViewModel()
         {
+            ListProxyFileItem = new ObservableCollection<string>();
 
+            #region команды
+            OpenFileCommand = new LambdaCommand(OnOpenFileCommandExecute, CanOpenFileCommandExecute);
+            ClearListCommand = new LambdaCommand(OnClearListCommandExecute, CanClearListCommandExecute);
+            #endregion
         }
 
         #endregion
---
+++ ViewModels/VMSettings/LocalProxySettings.cs
@@ -0,0 +1,31 @@
+﻿using BoxBoost.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxBoost.ViewModels.VMSettings
+{
+    [Serializable]
+    public abstract class LocalProxySettings : ViewModel
+    {
+        #region Настройка LocalProxy
+
+        #region Прокси из файла
+
+        /// <summary>Прокси из файла</summary>
+        private ObservableCollection<string> _ListProxyFileItem;
+
+        public ObservableCollection<string> ListProxyFileItem
+        {
+            get => _ListProxyFileItem;
+            set => Set(ref _ListProxyFileItem, value);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
