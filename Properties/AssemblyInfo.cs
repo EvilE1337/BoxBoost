972064eef8ce3ca72edf525434b321c4b5e881ed
Dmitry Pogonyiaev <lounge000@mail.ru>
2021-09-24 11:43:13 +0300

    Build+

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("BoxBoost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("E1337 Company")]
 [assembly: AssemblyProduct("BoxBoost")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.1.3")]
+[assembly: AssemblyFileVersion("2.1.1.3")]
+[assembly: NeutralResourcesLanguage("ru-KG")]
+
